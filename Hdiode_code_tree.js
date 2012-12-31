3220f1092b6f615c7c2b45ba8c53325aa6df723f
Nate Gemelke <ndg3@psu.edu>
2012-12-30 23:02:34 -0500

    First upload to github repository

---
+++ Hdiode_code_tree.js
@@ -0,0 +1,168 @@
+﻿
+/*jslint browser: true*/
+/*global $, jQuery, DOMParser, XMLSerializer, XSLTProcessor*/
+
+function Hdiode_code_tree(html_div, sources) {
+
+// This object implements a linked-pair of text/code-editor (using codeMirror) and HTML xml tree-editor.  The HTML representation of the tree is built using
+// an XSL transform which can be dynamically loaded/reloaded.  Optionally, an XSD schema can also be loaded.  Both editor and tree are inserted into the HTML DOM at the provided html_div.
+
+    function create_container(html_div) {
+    // creates child divisions to house title (topline), codemirror, and tree.  Creates textarea to later be converted into codemirror editor.
+        this.html_div_title = html_div.appendChild(document.createElement('div'));
+        this.html_div_title.setAttribute("class", "hdiode_xml_tree_titlediv");
+        this.html_div_title.appendChild(document.createElement('span')).appendChild(document.createTextNode('XML Editor'));
+        this.html_div_cm = html_div.appendChild(document.createElement('div'));
+        this.html_div_cm.setAttribute("class", "hdiode_xml_tree_cmdiv");
+        this.html_div_tree = html_div.appendChild(document.createElement('div'));
+        this.html_div_tree.setAttribute("class", "hdiode_xml_tree_treediv");
+        this.textarea = this.html_div_cm.appendChild(document.createElement('textarea'));
+        this.textarea.value = this.xml_string;
+    }
+    this.create_container = create_container;
+
+    function xmltoString(elem) {
+        var serialized, serializer;
+        try {
+            serializer = new XMLSerializer();
+            serialized = serializer.serializeToString(elem);
+        } catch (e) { serialized = elem.xml; }
+        return serialized;
+    }
+
+    function refresh_tree() {
+        //builds HTML tree by applying XSL to XML.
+        var xslparser, xsl, docparser, xml, xsltProcessor, ex, exs;
+        if (!this.xml_string) { return; }
+        if (!this.xsl_string) { return; }
+        xslparser = new DOMParser();
+        // -> would be good to avoid reparsing the xsl everytime.
+        if (!(typeof this.xslDoc === 'object')) {
+            this.xslDoc = xslparser.parseFromString(this.xsl_string, "text/xml");
+            }
+        docparser = new DOMParser();
+        xml = docparser.parseFromString(this.xml_string, "text/xml");
+        xsltProcessor = new XSLTProcessor();
+        xsltProcessor.importStylesheet(this.xslDoc);
+        ex = xsltProcessor.transformToFragment(xml, document);
+        exs = xmltoString(ex);
+        this.html_div_tree.appendChild(ex);
+        this.html_div_tree.innerHTML = exs; 
+        // -> need to bind update methods here - must require xsl routine to tag inputs for binding.
+        this.bind_events();
+    }
+    this.refresh_tree = refresh_tree;
+
+//    function toggleDiv_update_editor(event) {  //superceded/consumed by toggleProp
+//        // toggles an element between expanded and collapsed view by rewriting XML, and re-generating entire tree
+//        //retrieve XPATH to generating XML element from parent division gen_id property
+//        var elmpath = event.target.parentNode.getAttribute('gen_id').replace(/["']{1}/gi, "").split('divtree__')[1].replace(/__/g, "]/").replace(/_/g, "[") + "]";
+//        var docparser = new DOMParser();
+//        var xml = docparser.parseFromString(event.data.container.xml_string, "text/xml");
+//        var target = xml.evaluate(elmpath, xml, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext();
+//        ($(target).attr("expanded") == "1") ? $(target).attr("expanded", "0") : $(target).attr("expanded", "1");
+//        event.data.container.xml_string = xmltoString(xml);
+//        event.data.container.update_editor();
+//        // (tree is automatically refreshed by onchange event of codemirror editor)
+//        }
+//    this.toggleDiv_update_editor = toggleDiv_update_editor;
+
+    function toggleProp_update_editor(event) {
+         change_prop=event.data.args[0].replace(/["']{1}/gi, "");
+        // toggles an element between expanded and collapsed view by rewriting XML, and re-generating entire tree
+        //retrieve XPATH to generating XML element from first parent division's gen_id property
+        var elmpath = $(event.target).parents("div:first").get(0).getAttribute('gen_id').replace(/["']{1}/gi, "").split('divtree__')[1].replace(/__/g, "]/").replace(/_/g, "[") + "]";
+        var docparser = new DOMParser();
+        var xml = docparser.parseFromString(event.data.container.xml_string, "text/xml");
+        var target = xml.evaluate(elmpath, xml, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext();
+        var newval=($(target).attr(change_prop) === "1")?'0':'1';
+        ($(target).attr(change_prop) === "1") ? $(target).attr(change_prop, "0") : $(target).attr(change_prop, "1");
+        if (event.ctrlKey) {
+            //ctrl-toggle applies to children
+            targets = xml.evaluate(elmpath + "/*", xml, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+            for (var i = 0; i < targets.snapshotLength; i++) {
+                $(targets.snapshotItem(i)).attr(change_prop, newval);
+            }
+        }
+        if (event.altKey) {
+            //apply-toggle applies to all decendants
+            targets = xml.evaluate(elmpath + "//*", xml, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+            for (var i = 0; i < targets.snapshotLength; i++) {
+                $(targets.snapshotItem(i)).attr(change_prop, newval);
+            }
+        }
+        event.data.container.xml_string = xmltoString(xml);
+        event.data.container.update_editor();
+        // (tree is automatically refreshed by onchange event of codemirror editor)
+    }
+    this.toggleProp_update_editor = toggleProp_update_editor;
+
+    function updateElement_update_editor(event) {
+        // toggles an element between expanded and collapsed view by rewriting XML, and re-generating entire tree
+        //retrieve XPATH to generating XML element from first parent division's gen_id property
+        var elmpath=$(event.target).parents("div:first").get(0).getAttribute('gen_id') + '__' + event.target.name;
+        elmpath = elmpath.split('divtree__')[1].replace(/__/g, "]/").replace(/_/g, "[") + "]";
+        var docparser = new DOMParser();
+        var xml = docparser.parseFromString(event.data.container.xml_string, "text/xml");
+        var target = xml.evaluate(elmpath, xml, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext();
+        target.firstChild.data = event.target.value;
+        event.data.container.xml_string = xmltoString(xml);
+        event.data.container.update_editor();
+        // (tree is automatically refreshed by onchange event of codemirror editor)
+    }
+    this.updateElement_update_editor = updateElement_update_editor;
+
+
+    function bind_events() {
+        // this searches the html tree looking for xtsm_viewer_event attributes - their value
+        // should be of the form eventType:handlerFunctionName(arg1,arg2...)
+        // it then attaches the handler (should be a method of this object) to the HTML event
+        var bind_targets = document.evaluate('//*[@xtsm_viewer_event]', document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+        var next_target = bind_targets.iterateNext();
+        while (next_target) {
+            // this parses the event type and handler function from the xtsm_viewer_event attribute
+            var eventtype = next_target.getAttribute('xtsm_viewer_event').split(':')[0];
+            if (eventtype.substr(0, 2) === 'on') {
+                eventtype=eventtype.substr(2);
+                }
+            var handler_name = next_target.getAttribute('xtsm_viewer_event').split(':')[1].split('(')[0];
+            var handler_args = next_target.getAttribute('xtsm_viewer_event').split(':')[1].split('(')[1].split(')')[0].split(',');
+            if (typeof this[handler_name] === 'function') {
+                //this[handler_name].apply(this, handler_args);
+                //this line does the event-binding
+                var that = this;
+                $(next_target).on(eventtype, null, {container: this, args: handler_args} , this[handler_name]); 
+                }
+            next_target = bind_targets.iterateNext();
+        }
+    }
+    this.bind_events = bind_events;
+
+    function update_editor() { this.editor.setValue(this.xml_string); }
+    this.update_editor = update_editor;
+
+    function load_file(filename, target) {
+         var thatt = this;
+         $.get(filename, function (source) {
+             if (target === 'xml_string') { thatt.xml_string = source; thatt.update_editor(); return source; }
+             if (target === 'xsl_string') { thatt.xsl_string = source; return source; }
+             if (filename.split(/.xml|.xsd|.xtsm/).length > 1) { thatt.xml_string = source; thatt.update_editor(); return source; }
+             if (filename.split(/.xs|.xsl/).length > 1) { thatt.xsl_string = source; return source; }
+         }, 'text');
+    }
+    this.load_file = load_file;
+
+    this.xml_string = sources.xml_string;
+    this.xsl_string = sources.xsl_string;
+    this.xsd_string = sources.xsd_string;
+    if (html_div) { this.create_container(html_div); }
+    var that = this;
+    if (this.textarea) {
+        this.editor = CodeMirror.fromTextArea(this.textarea, { mode: "text/html", gutter: "True", lineNumbers: "True", gutters: ["note-gutter", "CodeMirror-linenumbers"], linewrapping: "True", autoCloseTags: true });
+        this.editor.on("change", function (inst, chObj) { that.xml_string = that.editor.getValue(); that.refresh_tree(); return; }); 
+        }
+    this.editor.setGutterMarker(0, "note-gutter", document.createTextNode("start>"));
+    return this;
+
+
+}
