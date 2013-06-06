b1b2e65c30957a0a2ecd23c2647172ba96839198
Jed Riebling <jedirieb@yahoo.com>
2013-06-06 18:43:20 -0300

    C header code for sparse-to-dense conversions.

---
+++ SCAtoDCA.h
@@ -0,0 +1,3 @@
+__declspec(dllexport) double tstodca44(unsigned int *returnarray,unsigned char *inputarray);  
+__declspec(dllexport) double tstodca24(unsigned short *returnarray,unsigned char *inputarray);  
+__declspec(dllexport) double tstodca14(unsigned char *returnarray,unsigned char *inputarray);  
